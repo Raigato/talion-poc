a0a474104ff5aecc41e72f8e0cc7cc9b6021e829
Raigato <gabriel.raymondou@gmail.com>
2024-05-18 11:22:58 +0200

    refactor: make live checker more obvious

--- src/components/LiveChecker/LiveCheckerDisplay.tsx
+++ src/components/LiveChecker/LiveCheckerDisplay.tsx
@@ -8,14 +8,18 @@ interface Props {
 const LiveCheckerDisplay = ({ isOnAir = false }: Props) => {
   return (
     <Link
-      className="flex items-center gap-4"
+      className="flex items-center gap-2"
       href={`https://twitch.tv/${process.env.NEXT_PUBLIC_TWITCH_USER_NAME}`}
     >
+      <span className="lg:hidden" aria-hidden>
+        Live
+      </span>
       <span
         className={cn(
           'h-6 w-6 rounded-full border border-white',
           isOnAir ? 'bg-red-500' : 'bg-neutral-400',
         )}
+        aria-hidden
       />
       <span className="sr-only lg:not-sr-only">
         {isOnAir ? 'Voir le stream' : 'Le stream est hors-ligne'}
